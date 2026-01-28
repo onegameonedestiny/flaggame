4640bd187a349529915ec89111f5bb0d3b8b0d7c
onegameonedestiny <153502552+onegameonedestiny@users.noreply.github.com>
2026-01-29 03:29:00 +0800

    Add files via upload

--- scripts/project/javaScriptInEvents.js
+++ scripts/project/javaScriptInEvents.js
@@ -7,14 +7,14 @@ const scriptsInEvents = {
 		runtime.JS_ListenRoomStatus()
 	},
 
-	async 共用_Event8_Act1(runtime, localVars)
-	{
-		runtime.JS_GetElapsedTime(runtime.globalVars.sofartime)
-	},
-
-	async 班長等待_Event2(runtime, localVars)
+	async 登入_Event8_Act1(runtime, localVars)
 	{
 		runtime.JS_GM_StartLevel("2")
+	},
+
+	async 顯示房_Event10_Act1(runtime, localVars)
+	{
+		runtime.JS_GetElapsedTime(runtime.globalVars.sofartime)
 	},
 
 	async 顯示房_Event7_Act1(runtime, localVars)
